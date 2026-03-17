c311c02c2d253ec3e3315b49c24661f9fd4e23f3
iRKSagar <97530589+iRKSagar@users.noreply.github.com>
2026-03-18 02:27:23 +0530

    Add R2 configuration variables to config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,9 @@
+R2_ACCOUNT_ID = "your_account_id"
 
+R2_ACCESS_KEY = "your_access_key"
+
+R2_SECRET_KEY = "your_secret_key"
+
+R2_BUCKET = "india20sixty-videos"
+
+R2_PUBLIC_URL = "https://pub-xxxx.r2.dev"
