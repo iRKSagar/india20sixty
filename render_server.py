dffd6dd25bef0404544c117896894eeb85f4267d
iRKSagar <97530589+iRKSagar@users.noreply.github.com>
2026-03-19 09:37:42 +0530

    Update render_server.py

--- render_server.py
+++ render_server.py
@@ -156,6 +156,8 @@ def upload_youtube(video_path, topic):
 
 # =============================
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
